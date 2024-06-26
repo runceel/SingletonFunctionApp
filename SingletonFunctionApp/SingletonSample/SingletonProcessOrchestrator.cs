0517d64b98f539455ccca065195519235ccc68dd
Kazuki Ota <kaota@microsoft.com>
2024-06-26 18:04:31 +0900

    remove

--- SingletonFunctionApp/SingletonSample/SingletonProcessOrchestrator.cs
+++ SingletonFunctionApp/SingletonSample/SingletonProcessOrchestrator.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.Azure.Functions.Worker;
 using Microsoft.DurableTask;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SingletonFunctionApp.SingletonSample;
 public class SingletonProcessOrchestrator
